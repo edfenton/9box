6cf0e6ef0ffa5670d8f493e26723b37dbf3709e7
Ed Fenton <edfenton@users.noreply.github.com>
2022-09-10 21:39:27 -0500

    Centering header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import logo from '../assets/logo192.png';
 const Header = () => {
 
   return (
-    <div className="d-flex align-items-center pt-3">
+    <div className="d-flex justify-content-center align-items-center pt-3">
       <img src={logo} height="50" alt="Logo number nine" />
       <p className="display-6">-Box Performance & Potential Matrix</p>
     </div>
